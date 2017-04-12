d96fdcdf3fa56b17e62bb8e649d9cf820aa3b3c8
Camille B <camille.bertuglia@insa-cvl.fr>
2017-04-12 13:30:52 +0200

    Fin partie 2

--- src/main/java/cb/App.java
+++ src/main/java/cb/App.java
@@ -1,5 +1,11 @@
 package cb;
 
+import com.opencsv.*;
+import java.io.*;
+import java.util.*;
+//import java.lang.*;
+
+
 /**
  * Hello world!
  *
@@ -16,7 +22,43 @@ public class App
 	public static void main( String[] args )
     {
         System.out.println( "Hello World! Quelques modifications !" );
-        int x = max(3,4);
-        System.out.println(x);
+        //int x = max(3,4);
+        //System.out.println(x);
+
+        int monmax = 0;
+        int x;
+        String[] nbstr;
+        String[] nb;
+        try
+        {
+            CSVReader reader = new CSVReader(new FileReader("data.csv"));
+            try
+            {
+                List<String[]>myEntries = reader.readAll();
+
+                for(int i=0; i<myEntries.size(); i++)
+                {
+                    System.out.println("Ligne:"+i);
+                    nbstr=myEntries.get(i);
+                    //parcourt
+                    nb=nbstr[0].split(";");
+                    for(int k=0; k<nb.length; k++)
+                    {
+                        x = Integer.parseInt(nb[k]);
+                        System.out.println("Nombre lu:"+x);
+                        monmax=max(x, monmax);
+                    }
+                }    
+            System.out.println("Le max:"+monmax);
+            }
+            catch (IOException e)
+            {
+                System.out.println("Erreur: IOException");
+            }
+        }
+        catch (FileNotFoundException ex)
+        {
+            System.out.println("Erreur: FileNotFoundException");
+        }
     }
 }
