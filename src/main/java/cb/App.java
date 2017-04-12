ff18dc9c99b593f0b691714d5f8ccd45e85bb762
Camille B <camille.bertuglia@insa-cvl.fr>
2017-04-12 15:41:11 +0200

    Fin de la partie 3

--- src/main/java/cb/App.java
+++ src/main/java/cb/App.java
@@ -3,8 +3,8 @@ package cb;
 import com.opencsv.*;
 import java.io.*;
 import java.util.*;
-//import java.lang.*;
-
+import org.apache.commons.collections.*;
+import java.lang.*;
 
 /**
  * Hello world!
@@ -16,8 +16,6 @@ public class App
     {
     	return a > b ? a:b;
     }
-	
-	
 	
 	public static void main( String[] args )
     {
@@ -34,26 +32,68 @@ public class App
             CSVReader reader = new CSVReader(new FileReader("data.csv"));
             try
             {
-                List<String[]>myEntries = reader.readAll();
-
-                for(int i=0; i<myEntries.size(); i++)
+                CSVWriter writer = new CSVWriter(new FileWriter("data-filtered.csv"), ';', (char)0);
+                try
                 {
-                    System.out.println("Ligne:"+i);
-                    nbstr=myEntries.get(i);
-                    //parcourt
-                    nb=nbstr[0].split(";");
-                    for(int k=0; k<nb.length; k++)
+                    List<String[]>myEntries = reader.readAll();
+
+
+                    for(int i=0; i<myEntries.size(); i++)
                     {
-                        x = Integer.parseInt(nb[k]);
-                        System.out.println("Nombre lu:"+x);
-                        monmax=max(x, monmax);
-                    }
-                }    
-            System.out.println("Le max:"+monmax);
+                        System.out.println("Ligne:"+i);
+                        nbstr=myEntries.get(i);
+                        nb=nbstr[0].split(";");
+                        List list = Arrays.asList(nb);
+                        Vector<String>out=new Vector<String>();
+                        Predicate predicat = new Predicate()
+                        {
+                            public boolean evaluate(Object x)
+                            {
+                                try
+                                {
+                                    return Integer.parseInt((String) x) < 50;
+                                }
+                                catch (java.lang.NumberFormatException n)
+                                {
+                                    return false;
+                                }
+                            }
+                        };
+                        
+                        CollectionUtils.select(list, predicat, out);
+                        System.out.println("OUT:"+out);
+                        for (int j=0; j < out.size(); j++) {
+                            String[] tmp=out.toArray(new String[0]);
+                            System.out.println("TMP:"+tmp[j]);
+                            if(j==out.size()-1)
+                            {
+                                writer.writeNext(tmp);
+                            }
+
+                            String y = out.get(j).toString();
+                            x= Integer.parseInt(y);
+                            monmax=max(x,monmax);
+                        }
+                        
+                        //parcourt
+                        //for(int k=0; k<nb.length; k++)
+                        //{
+                        //    x = Integer.parseInt(nb[k]);
+                        //    System.out.println("Nombre lu:"+x);
+                        //    monmax=max(x, monmax);
+                        //}
+                    }    
+                    System.out.println("Le max:"+monmax);
+                }
+                catch (IOException e)
+                {
+                    System.out.println("Erreur: IOException1");
+                }
+                writer.close();
             }
             catch (IOException e)
             {
-                System.out.println("Erreur: IOException");
+                System.out.println("Erreur: IOException2");
             }
         }
         catch (FileNotFoundException ex)
